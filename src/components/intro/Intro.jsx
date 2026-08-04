bf58727eac6717768b0f793d6f7a5cca71c0c30f
Albert <albertkepski3@gmail.com>
2026-08-04 11:01:16 +0200

    Refactor Intro section to use SplitSection and dynamic slider data

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,12 +5,16 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation } from "swiper/modules";
+
 import image1 from "../../assets/images/image1.png";
 import image2 from "../../assets/images/image2.png";
+import { silder } from "../../data/intro";
 
 import Arrow from "../ui/Arrow";
 import Button from "../ui/Button";
+import SplitSection from "../ui/SplitSection";
 
+const images = { image1, image2 };
 const Intro = () => {
   return (
     <section className="bg-beige relative">
@@ -25,61 +29,26 @@ const Intro = () => {
         }}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <div className="relative h-[85svh] lg:flex">
-            <div
-              className="absolute inset-0 bg-cover bg-center blur-[2px] h-[85svh] lg:hidden"
-              style={{ backgroundImage: `url(${image1})` }}
-            ></div>
+        {silder?.map(({ id, image, heading, description }) => (
+          <SwiperSlide key={id}>
+            <SplitSection image={images[image]} bgColor="bg-beige" opacity="50">
+              <h1 className="text-4xl/12.5 lg:text-6xl/17.5 font-medium">{heading}</h1>
 
-            <div className="absolute inset-0 bg-beige/50 h-[85svh] lg:hidden"></div>
+              <p className="mt-14 font text-base">{description}</p>
 
-            <div className="relative z-10 text-left p-10 text-lack drop-shadow-lg lg:flex-1 lg:flex lg:justify-center items-center">
-              <div className="max-w-150 ">
-                <h1 className="text-6xl/17.5 font-medium">Nowoczesna aranżacja Twojego ogrodu</h1>
+              <div className="flex flex-col gap-6 mt-10 sm:flex-row">
+                <Button className="bg-green text-grey border border-green hover:bg-beige hover:text-green">
+                  Skontaktuj się z nami
+                </Button>
 
-                <p className="mt-14 font text-base">
-                  Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji.
-                  Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.
-                </p>
-
-                <div className="flex flex-col gap-6 mt-10 sm:flex-row">
-                  <Button className="bg-green text-grey hover:bg-grey hover:text-green">
-                    Skontaktuj się z nami
-                  </Button>
-
-                  <Button className="flex bg-beige text-green border boder-beige sm:border-green hover:bg-green hover:text-beige">
-                    <span className="mr-2 flex-1">Zobacz nasze realizacje</span>
-                    <Arrow className="w-4 fill-green hover:fill-grey" />
-                  </Button>
-                </div>
+                <Button className="flex bg-beige text-green border boder-beige sm:border-green hover:bg-green hover:text-beige">
+                  <span className="mr-2 flex-1">Zobacz nasze realizacje</span>
+                  <Arrow className="w-4 fill-green hover:fill-grey" />
+                </Button>
               </div>
-            </div>
-            <img src={image1} alt="" className="hidden sm:block lg:flex-1" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="relative h-[85svh] lg:flex">
-            <div
-              className="absolute inset-0 bg-cover bg-center blur-[2px] lg:hidden"
-              style={{ backgroundImage: `url(${image2})` }}
-            ></div>
-
-            <div className="absolute inset-0 bg-black/20 lg:hidden"></div>
-
-            <div className="relative z-10 text-left p-10 text-lack drop-shadow-lg lg:flex-1 lg:flex lg:justify-center items-center">
-              <div className="max-w-150">
-                <h1 className="text-6xl font-medium">Nowoczesna aranżacja Twojego ogrodu</h1>
-
-                <p className="mt-14 text-base">
-                  Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji.
-                  Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.
-                </p>
-              </div>
-            </div>
-            <img src={image2} alt="" className="hidden sm:block lg:flex-1" />
-          </div>
-        </SwiperSlide>
+            </SplitSection>
+          </SwiperSlide>
+        ))}
       </Swiper>
 
       <div className="hidden md:block absolute right-0 bottom-[0%] z-4">

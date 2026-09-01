d44c84950948bd2c0c6b66caa6191f044f5143f9
Albert <albertkepski3@gmail.com>
2026-09-01 10:42:39 +0200

    Add footer content

---
+++ src/data/footer.js
@@ -0,0 +1,23 @@
+export const contactPrompt = "Daj znać, co możemy dla Ciebie zrobić!";
+export const footerCTA = "Skontaktuj się z nami";
+export const menuList = [
+  {
+    id: 1,
+    name: "Kontakt",
+  },
+  {
+    id: 2,
+    name: "Instagram",
+  },
+  {
+    id: 3,
+    name: "Facebook",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+  },
+];
+
+export const email = "giarddesign@kontakt.pl";
+export const phoneNumber = "000-000-000";

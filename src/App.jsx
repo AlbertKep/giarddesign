47987571565fb19a47cdcb47cc65e3ca193d80d8
Albert <albertkepski3@gmail.com>
2026-09-04 22:06:41 +0200

    Add main App layout with core sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import About from "./components/about/About";
+import Footer from "./components/footer/Footer";
+import InstagramBanner from "./components/instagramBanner/InstagramBanner";
+import Intro from "./components/intro/Intro";
+import Navbar from "./components/navbar/Navbar";
+import Offer from "./components/offer/Offer";
+import Projects from "./components/projects/Projects";
+
+Navbar;
 function App() {
   return (
     <>
-      <h1 className="bg-amber-300">hello World</h1>
+      <Navbar />
+      <Intro />
+      <Offer />
+      <About />
+      <Projects />
+      <InstagramBanner />
+      <Footer />
     </>
   );
 }

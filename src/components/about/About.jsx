6e178d543b98c362c7306c94719496e42140f594
Albert <albertkepski3@gmail.com>
2026-09-07 10:51:08 +0200

    Add slideUp animation and integrate updated SplitSection

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,26 +1,40 @@
+import { motion } from "motion/react";
+// data
 import { description, heading, label } from "../../data/about";
-
+// assets
 import image1 from "../../assets/images/image1.png";
+// ui
 import Arrow from "../ui/Arrow";
 import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import SplitSection from "../ui/SplitSection";
+// animations
+import { slideUp } from "../../motionVariants";
 
 const About = () => {
   return (
-    <SplitSection image={image1} bgColor="bg-green" opacity="50" reverse>
-      <Heading className="text-white mt-10" label={label} labelClassName="text-white">
-        {heading}
-      </Heading>
+    <SplitSection id="about" image={image1} bgColor="bg-green" overlay="bg-green/40" reverse>
+      <motion.div
+        variants={slideUp()}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{
+          once: true,
+          amount: 0.5,
+        }}
+      >
+        <Heading className="text-white mt-10" label={label} labelClassName="text-white">
+          {heading}
+        </Heading>
 
-      <p className="mt-14 font text-base text-white">{description}</p>
-
-      <div className="flex flex-col gap-6 mt-10 sm:flex-row ">
-        <Button className="flex bg-transparent text-white border border-white lg:bg-green hover:bg-green hover:text-beige">
-          <span className="mr-2 flex-1">Poznaj nas bliżej</span>
-          <Arrow className="w-4 fill-green hover:fill-grey -rotate-90" />
-        </Button>
-      </div>
+        <p className="mt-14 font text-base text-white">{description}</p>
+        <div className="flex flex-col gap-6 mt-10 sm:flex-row ">
+          <Button className="flex bg-transparent text-white border border-white lg:bg-green hover:bg-green hover:text-beige">
+            <span className="mr-2 flex-1">Poznaj nas bliżej</span>
+            <Arrow className="w-4 fill-green hover:fill-grey -rotate-90" />
+          </Button>
+        </div>
+      </motion.div>
     </SplitSection>
   );
 };

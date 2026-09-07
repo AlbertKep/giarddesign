3e749859f0a0ce86273931105710ed3028912140
Albert <albertkepski3@gmail.com>
2026-09-07 20:57:49 +0200

    Extract gallery images into separate data module

---
+++ src/data/gallery.js
@@ -0,0 +1,33 @@
+import image1 from "../assets/gallery/image1.png";
+import image10 from "../assets/gallery/image10.png";
+import image11 from "../assets/gallery/image11.png";
+import image12 from "../assets/gallery/image12.png";
+import image13 from "../assets/gallery/image13.png";
+import image14 from "../assets/gallery/image14.png";
+import image15 from "../assets/gallery/image15.png";
+import image2 from "../assets/gallery/image2.png";
+import image3 from "../assets/gallery/image3.png";
+import image4 from "../assets/gallery/image4.png";
+import image5 from "../assets/gallery/image5.png";
+import image6 from "../assets/gallery/image6.png";
+import image7 from "../assets/gallery/image7.png";
+import image8 from "../assets/gallery/image8.png";
+import image9 from "../assets/gallery/image9.png";
+
+export const images = [
+  { id: 1, image: image1 },
+  { id: 2, image: image2 },
+  { id: 3, image: image3 },
+  { id: 4, image: image4 },
+  { id: 5, image: image5 },
+  { id: 6, image: image6 },
+  { id: 7, image: image7 },
+  { id: 8, image: image8 },
+  { id: 9, image: image9 },
+  { id: 10, image: image10 },
+  { id: 11, image: image11 },
+  { id: 12, image: image12 },
+  { id: 13, image: image13 },
+  { id: 14, image: image14 },
+  { id: 15, image: image15 },
+];

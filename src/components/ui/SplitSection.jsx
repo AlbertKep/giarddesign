cc9fe53bfd0fc4504bfcbcd5aad325945e0034a9
Albert <albertkepski3@gmail.com>
2026-09-07 10:37:24 +0200

    Update SplitSection structure and props

--- src/components/ui/SplitSection.jsx
+++ src/components/ui/SplitSection.jsx
@@ -1,22 +1,23 @@
-const SplitSection = ({ children, image, bgColor, opacity, reverse }) => {
+const SplitSection = ({ id, children, image, bgColor, overlay, reverse }) => {
   return (
-    <div
-      className={`relative h-[85svh] lg:flex ${reverse ? "lg:flex-row-reverse" : "lg:flex-row"}`}
+    <section
+      id={id}
+      className={`relative z-0 h-[85svh] flex justify-center items-center sm:items-stretch ${reverse ? "lg:flex-row-reverse" : "lg:flex-row"}`}
     >
       <div
         className="absolute inset-0 bg-cover bg-center blur-[2px] h-[85svh] lg:hidden"
         style={{ backgroundImage: `url(${image})` }}
       ></div>
 
-      <div className={`absolute inset-0 ${bgColor}/${opacity} h-[85svh] lg:hidden`}></div>
+      <div className={`absolute inset-0 ${overlay} h-[85svh] lg:hidden`}></div>
 
       <div
-        className={`relative z-10 text-left p-10 text-lack drop-shadow-lg lg:flex-1 lg:flex lg:justify-center lg:${bgColor} items-center`}
+        className={`relative z-10 text-left p-10 text-lack drop-shadow-lg md:flex-1 lg:flex lg:justify-center lg:${bgColor} items-center`}
       >
         <div className="max-w-150 ">{children}</div>
       </div>
       <img src={image} alt={image} className="hidden lg:block lg:flex-1" />
-    </div>
+    </section>
   );
 };
 
